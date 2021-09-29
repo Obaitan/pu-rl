7ef3803eb34c2797ff27f03d2d67350030b4fad9
Obaitan <elero.obaitan@gmail.com>
2021-09-29 20:07:01 +0100

    Initialized project

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// Scroll to top script
+document.addEventListener("scroll", handleScroll);
+// get a reference to our predefined button
+var scrollToTopBtn = document.querySelector(".up");
+
+function handleScroll() {
+  var scrolled = window.pageYOffset;
+  if (scrolled > 400) {
+    //show button
+    scrollToTopBtn.style.display = "block";
+  } else {
+    //hide button
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+// End of script
+
+// Sticky Top Nav
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+      document.getElementById("header").classList.add("sticky-top");
+    } else {
+      document.getElementById("header").classList.remove("sticky-top");
+    }
+  });
+});
+
+// Animating Link Graphics
+function shortenLink() {
+  var a;
+  a = document.getElementById("link");
+  a.innerHTML = "<img src='images/svg/link6.svg' class='thumb' />";
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link5.svg' class='thumb' />";
+  }, 1000);
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link4.svg' class='thumb' />";
+  }, 2000);
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link3.svg' class='thumb' />";
+  }, 3000);
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link2.svg' class='thumb' />";
+  }, 4000);
+   setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link1.svg' class='thumb' />";
+  }, 5000);
+}
+shortenLink();
+setInterval(shortenLink, 6000);
+
+
