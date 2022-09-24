ef31a512390832d32458c553a3ec2cb1740a2a32
Obaitan <elero.obaitan@gmail.com>
2022-09-24 13:48:26 +0100

    Added a README file

---
+++ README.md
@@ -0,0 +1,12 @@
+# URL Shortner App
+
+A web application to shorten urls and generate qr-codes. The application is not currently connected to a backend so no sign-up is possible.
+
+## Installation
+
+It comprises PHP, CSS and JavaScript files so a server is required to run the PHP files.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+I, however, require users of the repository code to remove any personal information found in the code before using it. Thank you!
---
+++ js/script.js
@@ -0,0 +1,107 @@
+// Scroll to top script
+document.addEventListener("scroll", handleScroll);
+// get a reference to our predefined button
+var scrollToTopBtn = document.querySelector(".up");
+
+function handleScroll() {
+  var scrolled = window.pageYOffset;
+  if (scrolled > 400) {
+    //show button
+    scrollToTopBtn.style.display = "block";
+  } else {
+    //hide button
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+// End of script
+
+// Animate menu icon
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+// Sticky Top Nav
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+      document.getElementById("header").classList.add("sticky-top");
+    } else {
+      document.getElementById("header").classList.remove("sticky-top");
+    }
+  });
+});
+
+// Animating Link Graphics
+function shortenLink() {
+  var a;
+  a = document.getElementById("link");
+  a.innerHTML = "<img src='images/svg/link6.svg' class='thumb' />";
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link5.svg' class='thumb' />";
+  }, 900);
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link4.svg' class='thumb' />";
+  }, 1800);
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link3.svg' class='thumb' />";
+  }, 2700);
+  setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link2.svg' class='thumb' />";
+  }, 3600);
+   setTimeout(function () {
+    a.innerHTML = "<img src='images/svg/link1.svg' class='thumb' />";
+  }, 4500);
+}
+shortenLink();
+setInterval(shortenLink, 4500);
+
+
+var i = 0;
+var txt = 'https://.whatisthis.com/=?this-is-one-of-those-rather-long-and-hard-to-take-in-links?!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+var a = document.getElementById("shorten");
+var timer = 0
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("shorten").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } setTimeout(function () {
+    a.innerHTML = "whatisthis.com/find-out";
+  }, 5000);
+}
+
+typeWriter();
+// setInterval(typeWriter, 5000);
+
+// var intervalId = window.setInterval(function(){
+//   typeWriter();
+// }, 5000)
+
+// Contact Us Panel
+function openNav() {
+  document.getElementById("contact-panel").style.right = "0";
+}
+
+function closeNav() {
+  document.getElementById("contact-panel").style.right = "-700px";
+}
+
+
+// Limit input to the + sign and numbers only
+function isNumberKey(evt) {
+  var charCode = evt.which ? evt.which : event.keyCode;
+  if (charCode != 43 && charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+  return true;
+}
